d54438aa28e918a3bb97b8ae4bca8a0881e1f8df
Martin <martin.matusik@seznam.cz>
2024-03-17 13:33:18 +0100

    🚀 added joystick control

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 import socket
 import egm.egm_pb2 as abbegm
 from time import sleep
-import random
+import pygame
 
+# Sampling period in seconds
+dt = 0.1
+max_speed = 100
+
+offset_x = 0
+offset_y = 0
 
 ip_addr = "127.0.0.1"
 port = 6599
@@ -17,13 +23,25 @@ sensor_msg = abbegm.EgmSensor()
 sensor_msg.header.seqno = 0
 sensor_msg.header.mtype = abbegm.EgmHeader.MessageType.MSGTYPE_CORRECTION
 
+pygame.init()
+pygame.joystick.init()
+joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+
+print(joysticks[0].get_numaxes())
+
 if __name__ == '__main__':
 
     try:
         while True:
+            pygame.event.pump()           
+            offset_x = offset_x + joysticks[0].get_axis(1) * max_speed * dt
+            offset_y = offset_y + joysticks[0].get_axis(0) * max_speed * dt
+            #axis_x = joysticks[0].get_axis(0)
+            #axis_y = joysticks[0].get_axis(1)
+            #print("Poloha joysticku - X:", offset_x, "Y:", offset_y)
             sensor_msg.header.seqno += 1
-            sensor_msg.planned.cartesian.pos.x = 1000
-            sensor_msg.planned.cartesian.pos.y = random.uniform(-200, 200)
+            sensor_msg.planned.cartesian.pos.x = offset_x + 1000
+            sensor_msg.planned.cartesian.pos.y = offset_y 
             sensor_msg.planned.cartesian.pos.z = 350
             sensor_msg.planned.cartesian.euler.x = 0
             sensor_msg.planned.cartesian.euler.y = 0
@@ -32,7 +50,7 @@ if __name__ == '__main__':
             # msg, _ = socketUDP.recvfrom(1400)
             # robot_msg.ParseFromString(msg)
             # print(robot_msg.planned.cartesian.pos.x)
-            sleep(0.5)
+            sleep(dt)
 
     except KeyboardInterrupt:
         socketUDP.close()
